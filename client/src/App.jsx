33084bbeae0ec0e4aa99a8538d9fec67c8daad70
Rishab Kumar Jha <kumarjharishab@gmail.com>
2024-01-27 17:10:47 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/LandingPage/Login/Login';
-<<<<<<< HEAD
-import Register from './components/LandingPage/Register/Register';
-import { useState } from 'react';
-=======
 import HostProfile from './components/Host/HostProfile/HostProfile';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 5bca92d00d5073b57f60c2da31fa3f62e163f48b
+import Register from './components/LandingPage/Register/Register';
 
 function App() {
 
-  const [user , setUser] = useState({});
-  const token = localStorage.getItem("token");
-
-   
   return (
     <>
     <BrowserRouter>
       <Routes>
 
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/login" element={<Login />}></Route>\
+        <Route path="/signup" element={<Register />}></Route>\
         <Route path="/hostprofile" element={<HostProfile />}></Route>
       </Routes>
     </BrowserRouter>
