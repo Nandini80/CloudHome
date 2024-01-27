8c2b9d3ce22173609247b1c6ebe8a01ae2ec5435
Rishab Kumar Jha <kumarjharishab@gmail.com>
2024-01-27 15:52:07 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/LandingPage/Login/Login';
+import Register from './components/LandingPage/Register/Register';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path = '/signup' element= {<Register/>}></Route>
       </Routes>
     </BrowserRouter>
     </>
