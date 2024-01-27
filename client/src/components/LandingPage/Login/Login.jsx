83221d401e4f7ba4c4955cec96e6b238974e39c9
Nandini Jindal <nandinijindal010@gmail.com>
2024-01-27 14:19:18 +0530

    login updated

--- client/src/components/LandingPage/Login/Login.jsx
+++ client/src/components/LandingPage/Login/Login.jsx
@@ -1,37 +1,123 @@
-import './Login.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Img from "../../../assets/house1.jpg";
+import img2 from "../../../assets/login.jpg";
 
-function Login() {
+function Copyright(props) {
   return (
-    <div>
-      <div className="main-container">
-        <div className="heading">
-          <h2>Login to</h2>
-          <h1><span style={{ color: 'aqua' }}>Cloud</span>Home</h1>
-        </div>
-        <div className="login-container">
-          <h2>Login</h2>
-          <form className="login-form" id="loginForm">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" required />
-            </div>
-            <div className="form-group">
-              <button type="submit">Login</button>
-            </div>
-          </form>
-          <div className="message" id="message"></div>
-          <div className="forgot-register">
-            <a href="#">Forgot Password</a>
-            <a href="#">Register</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    </Typography>
   );
 }
 
-export default Login;
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <div>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" style={{marginLeft:"35%"}} sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${Img})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} style={{width:"100px",height:"100px",overflowY:"hidden"}}>
+              <img src={img2}  alt="" style={{maxWidth:"100%"}} />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log In
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Log In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2" style={{textDecoration:"none"}}>
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/Signup" variant="body2" style={{textDecoration:"none"}}>
+                    {"Register?"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+    </div>
+  );
+}
