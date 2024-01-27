5bca92d00d5073b57f60c2da31fa3f62e163f48b
Nandini Jindal <nandinijindal010@gmail.com>
2024-01-27 16:37:43 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/LandingPage/Login/Login';
-<<<<<<< HEAD
-=======
 import HostProfile from './components/Host/HostProfile/HostProfile';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 843b5211660df98380abff13bacc574dbe86fdd9
 
 function App() {
 
@@ -12,13 +9,10 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
+
         <Route path="/login" element={<Login />}></Route>
-=======
         <Route path="/login" element={<Login />}></Route>\
         <Route path="/hostprofile" element={<HostProfile />}></Route>
-
->>>>>>> 843b5211660df98380abff13bacc574dbe86fdd9
       </Routes>
     </BrowserRouter>
     </>
