ad62f0294076403eab6731d2c0ee3764c6208a67
Rishab Kumar Jha <kumarjharishab@gmail.com>
2024-01-28 08:02:20 +0530

    route

--- server/routers/Lands.js
+++ server/routers/Lands.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {createLand , deleteLand , editLand ,LandDetails} = require('../controllers/Lands');
-const {nearbyLand , rentedLands , getRentedUsers} = require('../controllers/getLands');
+const {nearbyLand , rentedLands , getRentedUsers , ownerCities} = require('../controllers/getLands');
 const { auth ,isOwner , isCustomer} = require('../middlewares/auth');
 
 router.post('/createLand'  ,  createLand);
@@ -13,5 +13,6 @@ router.get('/nearbyLands' ,  nearbyLand);
 router.get('/rentedLands' , auth , isCustomer , rentedLands);
 router.get('/getRentedUsers' ,auth , isOwner, getRentedUsers);
 router.get('/getLandsByCity' , getLandsByCity);
+router.get('/ownerCities' , ownerCities);
 
 module.exports = router;
